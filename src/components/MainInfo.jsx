a92248ae588c1bc63094c65ec6a3cd4918631181
Mike <miguel.suarezf05@gmail.com>
2023-05-02 16:50:08 -0500

    Created cards

--- src/components/MainInfo.jsx
+++ src/components/MainInfo.jsx
@@ -5,13 +5,34 @@ import Perfil from '../assets/perfil.jpg'
 
 const MainInfo = () => {
   return (
-    <section className="main__infoContainer">
-      <img src={Perfil} alt="Perfil Image" />
-      <div className="infoContainer__Text">
-        <h2>Mechatronic engineer/FrontEnd Developer</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid molestias dolor fugit earum eum facere in impedit tempore fuga. Placeat perspiciatis minima ea nostrum ab itaque sapiente explicabo, quasi ratione?</p>
-      </div>
-    </section>   
+    <>
+      <section className="main__infoContainer">
+        <img src={Perfil} alt="Perfil Image" />
+        <div className="infoContainer__Text">
+          <h2>Mechatronic engineer/FrontEnd Developer</h2>
+          <p>I'm Miguel Suarez and Programming is my passion</p>
+        </div>
+      </section>  
+      <section className="main__cardsContainer">
+        <div className="cardContainer">
+          <h2>Mechatronic engineer/FrontEnd Developer</h2>
+          <p>I'm Miguel Suarez and Programming is my passion</p>
+        </div>
+        <div className="cardContainer">
+          <h2>Mechatronic engineer/FrontEnd Developer</h2>
+          <p>I'm Miguel Suarez and Programming is my passion</p>
+        </div>
+        <div className="cardContainer">
+          <h2>Mechatronic engineer/FrontEnd Developer</h2>
+          <p>I'm Miguel Suarez and Programming is my passion</p>
+        </div>
+        <div className="cardContainer">
+          <h2>Mechatronic engineer/FrontEnd Developer</h2>
+          <p>I'm Miguel Suarez and Programming is my passion</p>
+        </div>
+      </section>
+    </>
+     
   )
 }
 
